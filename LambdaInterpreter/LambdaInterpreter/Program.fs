ede1e8f760d46b71d59234993c059e80379e7595
MikePuzanov <mikepuzanov@mail.ru>
2022-03-23 12:44:04 +0300

    add some tests

--- LambdaInterpreter/LambdaInterpreter/Program.fs
+++ LambdaInterpreter/LambdaInterpreter/Program.fs
@@ -3,9 +3,9 @@ open System.Collections.Generic
 open System.Security.Principal
 
 type Term =
-    | Variable of int
+    | Variable of string
     | Application of Term * Term
-    | LambdaAbstraction of int * Term
+    | LambdaAbstraction of string * Term
 
 let getVar term =
     let rec execute term list =
@@ -14,20 +14,21 @@ let getVar term =
             | Application(term1, term2) -> (execute term1 list) @ (execute term2 list)
             | LambdaAbstraction(v, term1) -> execute term1 (List.filter(fun x -> x <> v) list)
     execute term []
-let checkFreeVar var term = List.exists(fun x -> x = var) (getVar term)
+let checkFreeVar var term = term |> getVar |> List.exists(fun x -> x = var)
        
 let rec substitution var termOut termIn = 
     match termOut with
-    | Variable(v) -> if v = var then termOut else Variable(var)
+    | Variable(v) -> if v = var then termIn else Variable(var)
     | Application(termApp1, termApp2) -> Application(substitution var termApp1 termIn, substitution var termApp2 termIn)
-    | LambdaAbstraction(var1, term1) ->
-        match var1 with
-        | v when var = v -> LambdaAbstraction(var1, term1)
-        | v when (checkFreeVar var1 termIn) || (checkFreeVar var term1) -> LambdaAbstraction(var1, (substitution var term1 termIn))
+    | LambdaAbstraction(varInAbs, termInAbs) ->
+        match varInAbs with
+        | v when var = v -> LambdaAbstraction(varInAbs, termInAbs)
+        | v when (checkFreeVar varInAbs termIn) || (checkFreeVar var termInAbs) -> LambdaAbstraction(varInAbs, (substitution var termInAbs termIn))
         | _ ->
-            //
-            //
-            //
+            let newVar = (((getVar termInAbs) @ (getVar termIn)) |> List.toArray |> Array.max) + "1"
+            let newTerm = newVar |> Variable
+            let termWithNewVar = substitution var termInAbs newTerm 
+            LambdaAbstraction(newVar, substitution var termWithNewVar termIn )
             
 
 let rec betaReduction term =
