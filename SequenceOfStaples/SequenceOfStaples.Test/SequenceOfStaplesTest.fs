ec8202031d72310c758ef23f9751ef163d644e4e
MikePuzanov <mikepuzanov@mail.ru>
2022-03-24 20:31:27 +0300

    add tests

--- SequenceOfStaples/SequenceOfStaples.Test/SequenceOfStaplesTest.fs
+++ SequenceOfStaples/SequenceOfStaples.Test/SequenceOfStaplesTest.fs
@@ -1,11 +1,25 @@
 module SequenceOfStaples.Test
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open FsUnit
+open Program
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let TestExpTrue () =
+    let result = checkStaples "(({}))"
+    result |> should equal true
+    
+[<Test>]
+let TestExpWrongWithOneMoreStaple () =
+    let result = checkStaples "(({})))"
+    result |> should equal false
+    
+[<Test>]
+let TestExpWrongWithDifferentSides () =
+    let result = checkStaples ")("
+    result |> should equal false
+    
+[<Test>]
+let TestExpWrongWithDifferentStaple () =
+    let result = checkStaples "(]"
+    result |> should equal false
--- SequenceOfStaples/SequenceOfStaples/Program.fs
+++ SequenceOfStaples/SequenceOfStaples/Program.fs
@@ -1,16 +1,12 @@
-//Реализовать функцию, которая по произвольной строке проверяет корректность скобочной
-//последовательности в этой строке. Скобки бывают трёх видов.
-
 let checkStaples (exp : string) =
-    let arrayChar = Seq.toList exp
-    let rec check (listOfExp : char List) (array : char array) =
+    let ListOfChar = Seq.toList exp
+    let rec check (listOfExp : char List) (listOfStaples : char list) =
+        let charFunc x = if x = listOfStaples.Head then (check listOfExp.Tail listOfStaples.Tail) else false
         match listOfExp with
-        | h :: listOfExp when h = '[' || h = '(' || h = '{' -> (check listOfExp.Tail (array |> Array.insertAt 0 h)) 
-        | h :: listOfExp when h = ']' -> if '[' = (array |> Array.item 0) then (check listOfExp.Tail (array |> Array.removeAt 0)) else false
-        | h :: listOfExp when h = ')'-> if '(' = (array |> Array.item 0) then (check listOfExp.Tail (array |> Array.removeAt 0)) else false
-        | h :: listOfExp when h = '}' -> if '{' = (array |> Array.item 0) then (check listOfExp.Tail (array |> Array.removeAt 0)) else false
-        | [] -> array.Length = 0
+        | h :: tail when h = '[' || h = '(' || h = '{' -> (check tail (h :: listOfStaples)) 
+        | h :: listOfExp when h = ']' && listOfStaples.Length > 0 -> charFunc '['
+        | h :: listOfExp when h = ')' && listOfStaples.Length > 0 -> charFunc '('
+        | h :: listOfExp when h = '}' && listOfStaples.Length > 0 -> charFunc '{'
+        | [] -> listOfStaples.Length = 0
         | _ -> false
-    check arrayChar [||]
-
-if (checkStaples "(()){}") then printf "true" else printf "false"  
+    check ListOfChar []
