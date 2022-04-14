5f4864e2028bad87e831aa59a39214ea273ec12a
MikePuzanov <mikepuzanov@mail.ru>
2022-04-14 15:17:03 +0300

    test

---
+++ Test/Test/HashTable.fs
@@ -0,0 +1,11 @@
+﻿module Test.HashTable
+
+open System
+open Test
+
+type public MyHashTable (func : Func<string, int>) =
+    let func element = func 
+    let hash (element : string) = element |> func
+    
+  
+        
---
+++ Test/Test/MinInList.fs
@@ -0,0 +1,5 @@
+﻿module Test.MinInList
+
+// находит минимальный элемент списка
+let minInList list =
+    (list |> List.map (fun x -> x * -1) |> List.max ) * -1
