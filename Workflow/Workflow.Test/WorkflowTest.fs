407befd6e0d60eb54ca8cbfc4f5373f63fd1c518
MikePuzanov <mikepuzanov@mail.ru>
2022-04-26 01:33:20 +0300

    workflow

---
+++ Workflow/Workflow.Test/WorkflowTest.fs
@@ -0,0 +1,51 @@
+module Workflow.Test
+
+open System
+open NUnit.Framework
+open FsUnit
+open Program
+
+[<Test>]
+let RoundTest () =
+    let rounding i = new RoundBuilder(i)
+    let result =
+        rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    result |> should equal 0.048
+    
+[<Test>]
+let RoundTestWithException () =
+    let rounding i = new RoundBuilder(i)
+    (fun() ->
+        rounding -1 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    } |> ignore) |> should throw typeof<ArgumentOutOfRangeException>
+    
+[<Test>]
+let CalculateTest () =
+    let calculate = new CalculateBuilder()
+    let result =
+        calculate {
+        let! a = "1"
+        let! b = "3"
+        let z = a + b
+        return z
+    }
+    result |> should equal (Some 4)
+    
+[<Test>]
+let CalculateTestIncorrect () =
+    let calculate = new CalculateBuilder()
+    let result =
+        calculate {
+        let! a = "1"
+        let! b = "."
+        let z = a + b
+        return z
+    }
+    result |> should equal None
---
+++ Workflow/Workflow/Program.fs
@@ -0,0 +1,18 @@
+open System
+
+
+type RoundBuilder(i) =
+    let debug (x : float) (i : int) = Math.Round(x, i)
+    member this.Bind(x, f) =
+        f (debug x i)
+    member this.Return(x : float) = Math.Round(x, i)
+    
+type CalculateBuilder() =     
+    member this.Bind(x : string, f) =
+        try
+            match Int32.TryParse x with
+            | true, x -> f x
+            | false, x -> raise(ArgumentException())
+        with
+            | :? ArgumentException -> None
+    member this.Return(x) = Some x
